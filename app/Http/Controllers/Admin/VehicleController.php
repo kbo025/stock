31dc66b7b15e7f04127053c58030cac9a2f51468
Gabriel Camacho <kbo025@gmail.com>
2021-12-16 22:26:13 -0300

    feat: modificações nas views

--- app/Http/Controllers/Admin/VehicleController.php
+++ app/Http/Controllers/Admin/VehicleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\VehicleRequest; //TODO
 use App\Http\Controllers\BackendController;
 use App\Models\Vehicle;
+use Yajra\Datatables\Datatables;
 
 class VehicleController extends BackendController
 {
@@ -25,7 +26,7 @@ class VehicleController extends BackendController
      */
     public function index()
     {
-        return view('admin.vehicles.index', $this->data);
+        return view('admin.vehicle.index', $this->data);
     }
 
     /**
@@ -35,7 +36,7 @@ class VehicleController extends BackendController
      */
     public function create()
     {
-        return view('admin.vehicles.create', $this->data);
+        return view('admin.vehicle.create', $this->data);
     }
 
     /**
@@ -58,7 +59,7 @@ class VehicleController extends BackendController
         $vehicle->save();
 
 
-        return redirect(route('admin.vehicles.index'))->withSuccess('Veiculo registrado com sucesso');
+        return redirect(route('admin.vehicle.index'))->withSuccess('Veiculo registrado com sucesso');
     }
 
     /**
@@ -70,7 +71,7 @@ class VehicleController extends BackendController
     public function show($id)
     {
         $this->data['vehicle'] = Vehicle::findOrFail($id);
-        return view('admin.vehicles.show', $this->data);
+        return view('admin.vehicle.show', $this->data);
     }
 
     /**
@@ -82,7 +83,7 @@ class VehicleController extends BackendController
     public function edit($id)
     {
         $this->data['vehicle'] = Vehicle::findOrFail($id);
-        return view('admin.vehicles.edit', $this->data);
+        return view('admin.vehicle.edit', $this->data);
     }
 
     /**
@@ -103,7 +104,7 @@ class VehicleController extends BackendController
         // $vehicle->address    = strip_tags($request->address);
         $vehicle->save();
 
-        return redirect(route('admin.vehicles.index'))->withSuccess('Dados atualizados com sucesso');
+        return redirect(route('admin.vehicle.index'))->withSuccess('Dados atualizados com sucesso');
 
     }
 
@@ -118,85 +119,78 @@ class VehicleController extends BackendController
         $vehicle = Vehicle::findOrFail($id);
         if ((auth()->id() == 1)) {
             $vehicle->delete();
-            return redirect(route('admin.vehicles.index'))->withSuccess('Registro eliminado com sucesso');
+            return redirect(route('admin.vehicle.index'))->withSuccess('Registro eliminado com sucesso');
         }
     }
 
-    // public function getAdministrators()
-    // {
-    //     $role           = Role::find(1);
-    //     $roleTow        = Role::find(4);
-    //     $users     = User::role([$role->name,$roleTow->name])->latest()->get();
-    //     $userArray = [];
+    public function getVehicles()
+    {
+        // $modles     = Vehicle::role([$role->name,$roleTow->name])->latest()->get();
+        $modelArray = [];
 
-    //     $i = 1;
-    //     if (!blank($users)) {
-    //         foreach ($users as $user) {
-    //             $userArray[$i]          = $user;
-    //             $userArray[$i]['setID'] = $i;
-    //             $i++;
-    //         }
-    //     }
-    //     return Datatables::of($userArray)
-    //         ->addColumn('action', function ($user) {
-    //             $retAction = '';
-    //             if (($user->id == auth()->id()) && (auth()->id() == 1)) {
-    //                 if (auth()->user()->can('drivers_show')) {
-    //                     $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
-    //                 }
+        // $i = 1;
+        // if (!blank($users)) {
+        //     foreach ($users as $user) {
+        //         $userArray[$i]          = $user;
+        //         $userArray[$i]['setID'] = $i;
+        //         $i++;
+        //     }
+        // }
+        return Datatables::of($modelArray)
+            ->escapeColumns([])
+            ->make(true);
 
-    //                 if (auth()->user()->can('drivers_edit')) {
-    //                     $retAction .= '<a href="' . route('admin.vehicles.edit', $user) . '" class="btn btn-sm btn-icon float-left btn-primary ml-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="far fa-edit"></i></a>';
-    //                 }
-    //             } else if (auth()->id() == 1) {
-    //                 if (auth()->user()->can('drivers_show')) {
-    //                     $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
-    //                 }
+            // ->addColumn('action', function ($user) {
+            //     $retAction = '';
+            //     if (($user->id == auth()->id()) && (auth()->id() == 1)) {
+            //         if (auth()->user()->can('vehicles_show')) {
+            //             $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
+            //         }
 
-    //                 if (auth()->user()->can('drivers_edit')) {
-    //                     $retAction .= '<a href="' . route('admin.vehicles.edit', $user) . '" class="btn btn-sm btn-icon float-left btn-primary ml-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="far fa-edit"></i></a>';
-    //                 }
+            //         if (auth()->user()->can('vehicles_edit')) {
+            //             $retAction .= '<a href="' . route('admin.vehicles.edit', $user) . '" class="btn btn-sm btn-icon float-left btn-primary ml-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="far fa-edit"></i></a>';
+            //         }
+            //     } else if (auth()->id() == 1) {
+            //         if (auth()->user()->can('vehicles_show')) {
+            //             $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
+            //         }
 
-    //                 if (auth()->user()->can('drivers_delete')) {
-    //                     $retAction .= '<form class="float-left pl-2" action="' . route('admin.vehicles.destroy', $user) . '" method="POST">' . method_field('DELETE') . csrf_field() . '<button class="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button></form>';
-    //                 }
-    //             } else {
-    //                 if ($user->id == 1) {
-    //                     if (auth()->user()->can('drivers_show')) {
-    //                         $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
-    //                     }
-    //                 } else {
-    //                     if (auth()->user()->can('drivers_show')) {
-    //                         $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
-    //                     }
+            //         if (auth()->user()->can('vehicles_edit')) {
+            //             $retAction .= '<a href="' . route('admin.vehicles.edit', $user) . '" class="btn btn-sm btn-icon float-left btn-primary ml-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="far fa-edit"></i></a>';
+            //         }
 
-    //                     if (auth()->user()->can('drivers_edit')) {
-    //                         $retAction .= '<a href="' . route('admin.vehicles.edit', $user) . '" class="btn btn-sm btn-icon float-left btn-primary ml-2"><i class="far fa-edit"></i></a>';
-    //                     }
-    //                 }
-    //             }
+            //         if (auth()->user()->can('vehicles_delete')) {
+            //             $retAction .= '<form class="float-left pl-2" action="' . route('admin.vehicles.destroy', $user) . '" method="POST">' . method_field('DELETE') . csrf_field() . '<button class="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button></form>';
+            //         }
+            //     } else {
+            //         if ($user->id == 1) {
+            //             if (auth()->user()->can('vehicles_show')) {
+            //                 $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
+            //             }
+            //         } else {
+            //             if (auth()->user()->can('vehicles_show')) {
+            //                 $retAction .= '<a href="' . route('admin.vehicles.show', $user) . '" class="btn btn-sm btn-icon float-left btn-info" data-toggle="tooltip" data-placement="top" title="View"><i class="far fa-eye"></i></a>';
+            //             }
 
-    //             return $retAction;
-    //         })
-    //         ->addColumn('image', function ($user) {
-    //             return '<figure class="avatar mr-2"><img src="' . $user->images . '" alt=""></figure>';
-    //         })
-    //         ->addColumn('name', function ($user) {
-    //             return $user->name;
-    //         })
-    //         ->addColumn('role', function ($user) {
-    //             return $user->getrole->name;
-    //         })
-    //         ->editColumn('id', function ($user) {
-    //             return $user->setID;
-    //         })
-    //         ->escapeColumns([])
-    //         ->make(true);
-    // }
+            //             if (auth()->user()->can('vehicles_edit')) {
+            //                 $retAction .= '<a href="' . route('admin.vehicles.edit', $user) . '" class="btn btn-sm btn-icon float-left btn-primary ml-2"><i class="far fa-edit"></i></a>';
+            //             }
+            //         }
+            //     }
 
-    // private function username($email)
-    // {
-    //     $emails = explode('@', $email);
-    //     return $emails[0] . mt_rand();
-    // }
+            //     return $retAction;
+            // })
+            // ->addColumn('image', function ($user) {
+            //     return '<figure class="avatar mr-2"><img src="' . $user->images . '" alt=""></figure>';
+            // })
+            // ->addColumn('name', function ($user) {
+            //     return $user->name;
+            // })
+            // ->addColumn('role', function ($user) {
+            //     return $user->getrole->name;
+            // })
+            // ->editColumn('id', function ($user) {
+            //     return $user->setID;
+            // })
+    }
 }
